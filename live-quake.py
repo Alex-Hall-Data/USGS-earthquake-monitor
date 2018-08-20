faca7061bb1680aa8742569a86dd3092521661a6
alex-hall-data <northern.bantam@gmail.com>
2018-08-20 19:07:12 +0000

    fixed indexing issues

--- live-quake.py
+++ live-quake.py
@@ -7,13 +7,34 @@ Created on Mon Aug 20 13:15:55 2018
 
 from quakefeeds import QuakeFeed
 import time
+import math
+import Adafruit_CharLCD as LCD
 
+#setup lcd
+
+# Raspberry Pi pin configuration:
+lcd_rs        = 27  # Note this might need to be changed to 21 for older revision Pi's.
+lcd_en        = 22
+lcd_d4        = 25
+lcd_d5        = 24
+lcd_d6        = 23
+lcd_d7        = 18
+lcd_backlight = 4
+
+
+# Define LCD column and row size for 16x2 LCD.
+lcd_columns = 16
+lcd_rows    = 2
+
+#initialise the lcd
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, 
+                           lcd_columns, lcd_rows, lcd_backlight)
 
 while(True):
     
     #query api and retrieve data for last 24 hours
     feed = QuakeFeed("4.5", "day")
-    last_quake = feed[len(feed)-1]
+    last_quake = feed[0]
     
     title = last_quake.get('properties').get('title')
     magnitude = last_quake.get('properties').get('mag')
